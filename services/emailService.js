8580cfc39dddd05259136f0415039f4441d47f50
malhaouit <abdelmounaim.abdel@gmail.com>
2024-11-07 07:06:06 +0000

    Add visit us

--- services/emailService.js
+++ services/emailService.js
@@ -4,13 +4,16 @@ require('dotenv').config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendConfirmationEmail(email) {
+  const websiteUrl = 'https://www.skilledpartner.com';
+
   const msg = {
     to: email,
     from: 'info@skilledpartner.com', // Use your verified SendGrid sender email
     subject: 'Thank you for subscribing!',
-    text: `Hello! Thanks for subscribing to our pre-launch updates. We’ll notify you when we launch`,
-    html: `<p>Hello! Thanks for subscribing to our pre-launch updates. We’ll notify you when we launch</p>`,
+    text: `Hello! Thanks for subscribing to our pre-launch updates. We’ll notify you when we launch. Visit us at ${websiteUrl}`,
+    html: `<p>Hello! Thanks for subscribing to our pre-launch updates. We’ll notify you when we launch. Visit us at <a href="${websiteUrl}" target="_blank">${websiteUrl}</a></p>`,
   };
+  
   try {
     await sgMail.send(msg);
     console.log(`Confirmation email sent to ${email}`);
